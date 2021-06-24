7568062434709c1a0909e9f730e392c13f1ab6f7
Sabrina Sides <sabrinasides91@gmail.com>
2021-06-23 19:38:58 -0500

    adding swiping app info to landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,24 +1,77 @@
-import React from 'react';
-import '../styles/LandingPage.css'
-import NurseTyping from '../images/nurse-typing.jpg'
+import React, { useRef, useState } from 'react';
+import '../styles/LandingPage.css';
+import NurseTyping from '../images/nurse-typing.jpg';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper.min.css';
+import 'swiper/components/navigation/navigation.min.css';
+import '../styles/LandingSwiper.css';
+import SwiperCore, { Navigation } from 'swiper/core';
+
+SwiperCore.use([Navigation]);
 
 function LandingPage() {
   return (
     <div>
-      <div className="features-block">
-      <h2>Skip the paper and pen- create and manage patient reports</h2>
-      <img src={NurseTyping} alt='nurse typing on laptop'/>
-        <div className="feature">
+      <section className='landing-header'>
+        <h2>
+          Skip the paper and pen- create and manage patient reports on your
+          phone and/or computer
+        </h2>
+        <img src={NurseTyping} alt='nurse typing on laptop' />
+      </section>
+      {/* <div className='features-block-1'>
+        <div className='feature'>
           <h3>Keep all your patient reports in one place</h3>
-          <p>Easily access multiple reports from one page</p>
+          <p>Easily access all of your reports from the home page</p>
         </div>
-        <div className="feature">
-          <h3>Quickly create, edit and delete reports</h3>
-          <p>Manage your report needs at the click of a button</p>
+        <div className='feature'>
+          <h3>Quick access to patient info</h3>
+          <p>Switch between reports with one click on the desired tab</p>
         </div>
       </div>
+      <div className='features-block-2'>
+        <div className='feature'>
+          <h3>Manage your reports neatly</h3>
+          <p>
+            Create, edit and delete reports without smudging ink with crossed
+            out info
+          </p>
+        </div>
+        <div className='feature'>
+          <h3>Designed by a nurse for nurses</h3>
+          <p>
+            Created to help ease the stress of keeping track of your "brains" so
+            you can take better care of your patients
+          </p>
+        </div>
+      </div> */}
+      <>
+        <Swiper navigation={true} className='mySwiper'>
+          <SwiperSlide><div className='feature'>
+          <h3>Keep all your patient reports in one place</h3>
+          <p>Easily access all of your reports from the home page</p>
+        </div></SwiperSlide>
+          <SwiperSlide><div className='feature'>
+          <h3>Quick access to patient info</h3>
+          <p>Switch between reports with one click on the desired tab</p>
+        </div></SwiperSlide>
+          <SwiperSlide><div className='feature'>
+          <h3>Manage your reports neatly</h3>
+          <p>
+            Create, edit and delete reports without smudging ink with crossed
+            out info
+          </p>
+        </div></SwiperSlide>
+          <SwiperSlide> <div className='feature'>
+          <h3>Designed by a nurse for nurses</h3>
+          <p>
+            Created to help ease the stress of keeping track of your "brains" so
+            you can take better care of your patients
+          </p>
+        </div></SwiperSlide>
 
-      
+        </Swiper>
+      </>
     </div>
   );
 }
